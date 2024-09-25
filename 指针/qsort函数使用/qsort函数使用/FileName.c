656676d4cba69d885a414e72792c358f19112ee2
MerrikLing <MerrikLing@outlook.com>
2024-09-25 23:18:25 +0800

    qsort排列结构体

--- 指针/qsort函数使用/qsort函数使用/FileName.c
+++ 指针/qsort函数使用/qsort函数使用/FileName.c
@@ -1,8 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<stdlib.h>
+struct stu
+{
+	char name[20];
+	int money;
+};
 int cmp(const void* e1, const void* e2)
 {
 	return *(int*)e1 - *(int*)e2;
+}
+int m_cmp(const void* e1, const void* e2)
+{
+	return (((struct stu*)e1)->money) - ((struct stu*)e2)->money;
 }
 int main()
 {
@@ -12,5 +22,13 @@ int main()
 	int i = 0;
 	for (i = 0;i < sz;i++)
 		printf("%d ", a[i]);
+	
+	printf("\n");
+	struct stu s[] = { {"张三",250},{"李四",10} ,{"王五",20} };
+	int sz_s = sizeof(s) / sizeof(s[0]);
+	qsort(s, sz_s, sizeof(s[0]), m_cmp);
+	for (int j = 0;j < sz_s;j++)
+		printf("%s %d  ", s[j].name, s[j].money);
+
 	return 0;
 }
